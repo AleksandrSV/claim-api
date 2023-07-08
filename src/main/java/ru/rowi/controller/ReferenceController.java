6468af9f19f3b59eda1770d83aa8abef05296020
Vladislav <watislove4u@gmai.com>
2023-07-09 03:07:10 +0400

    complete ReferenceController.java

--- src/main/java/ru/rowi/controller/ReferenceController.java
+++ src/main/java/ru/rowi/controller/ReferenceController.java
@@ -1,15 +1,23 @@
 package ru.rowi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.rowi.dto.reference.ReferenceResponse;
+
+import javax.persistence.Access;
 
 @RestController
 @RequestMapping("/references")
 public class ReferenceController {
-    @GetMapping
-    public ResponseEntity<?> getReferences(){
-        return ResponseEntity.ok("references");
+    @Autowired
+    ReferenceResponse referenceResponse;
+
+    @GetMapping(produces = "application/json")
+    public ResponseEntity<?> getReferences() {
+        return ResponseEntity.ok(referenceResponse);
     }
+
 }
